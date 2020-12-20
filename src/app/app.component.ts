92fc718146d925230d91beb7d7e322aae913614f
podpage <support@podpage.org>
2020-12-20 21:44:24 +0100

    created template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'template';
+
+  constructor(router: Router) {
+    console.log(router.config);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ApplicationSetupProvider, NuntCmsModule, NuntCoreModule} from '@nunt/core';
+import {NuntDesignModule} from '@nunt/design';
+import {RouterModule} from '@angular/router';
+import {routes} from './app.routes';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NuntCoreModule.forRoot({
+      host: 'https://stage.nunt.io',
+      apiUrl: 'https://stage.nunt.io'
+    }),
+    NuntDesignModule.forRoot(),
+    NuntCmsModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ApplicationSetupProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
