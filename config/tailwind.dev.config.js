052d081b65b0904b686d1da233033343aa1b89d9
podpage <support@podpage.org>
2020-12-21 04:14:01 +0100

    fixed tailwind config

--- config/tailwind.dev.config.js
+++ config/tailwind.dev.config.js
@@ -1,40 +1,10 @@
+const config = require('@nunt/design/tailwind.config');
+
 module.exports = {
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        mute: 'var(--color-mute)',
-        boundary: 'var(--color-boundary)',
-        diff: 'var(--color-diff)',
-        blank: 'var(--color-blank)',
-        common: 'var(--color-common)',
-        accent: 'var(--color-accent)'
-      },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out',
-      },
-      keyframes: {
-        wiggle: {
-          '0%': {transform: 'translate(1px, 1px) rotate(0deg)'},
-          '10%': {transform: 'translate(-1px, -2px) rotate(-1deg)'},
-          '20%': {transform: 'translate(-2px, 0px) rotate(1deg)'},
-          '30%': {transform: 'translate(3px, 2px) rotate(0deg)'},
-          '40%': {transform: 'translate(1px, -1px) rotate(1deg)'},
-          '50%': {transform: 'translate(-1px, 2px) rotate(-1deg)'},
-          '60%': {transform: 'translate(-2px, 1px) rotate(0deg)'},
-          '70%': {transform: 'translate(2px, 1px) rotate(-1deg)'},
-          '80%': {transform: 'translate(-1px, -1px) rotate(1deg)'},
-          '90%': {transform: 'translate(1px, 2px) rotate(0deg)'},
-          '100%': {transform: 'translate(0, 0) rotate(0deg)'}
-        }
-      }
-    }
-  },
-  corePlugins: {
-    gradientColorStops: false,
-  },
-  variants: {},
-  plugins: [],
-}
+  darkMode: config.darkMode,
+  purge: config.purge,
+  theme: config.theme,
+  corePlugins: config.corePlugins,
+  variants: config.variants,
+  plugins: config.plugins
+};
--- config/tailwind.prod.config.js
+++ config/tailwind.prod.config.js
@@ -1,5 +1,7 @@
+const config = require('@nunt/design/tailwind.config');
+
 module.exports = {
-  darkMode: 'class',
+  darkMode: config.darkMode,
   purge: {
     enabled: true,
     content: [
@@ -8,41 +10,8 @@ module.exports = {
       './node_modules/@nunt/**/**/*.js'
     ],
   },
-  theme: {
-    extend: {
-      colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        mute: 'var(--color-mute)',
-        boundary: 'var(--color-boundary)',
-        diff: 'var(--color-diff)',
-        blank: 'var(--color-blank)',
-        common: 'var(--color-common)',
-        accent: 'var(--color-accent)'
-      },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out',
-      },
-      keyframes: {
-        wiggle: {
-          '0%': {transform: 'translate(1px, 1px) rotate(0deg)'},
-          '10%': {transform: 'translate(-1px, -2px) rotate(-1deg)'},
-          '20%': {transform: 'translate(-2px, 0px) rotate(1deg)'},
-          '30%': {transform: 'translate(3px, 2px) rotate(0deg)'},
-          '40%': {transform: 'translate(1px, -1px) rotate(1deg)'},
-          '50%': {transform: 'translate(-1px, 2px) rotate(-1deg)'},
-          '60%': {transform: 'translate(-2px, 1px) rotate(0deg)'},
-          '70%': {transform: 'translate(2px, 1px) rotate(-1deg)'},
-          '80%': {transform: 'translate(-1px, -1px) rotate(1deg)'},
-          '90%': {transform: 'translate(1px, 2px) rotate(0deg)'},
-          '100%': {transform: 'translate(0, 0) rotate(0deg)'}
-        }
-      }
-    }
-  },
-  corePlugins: {
-    gradientColorStops: false,
-  },
-  variants: {},
-  plugins: [],
-}
+  theme: config.theme,
+  corePlugins: config.corePlugins,
+  variants: config.variants,
+  plugins: config.plugins
+};
