805a208548f49841d95261db6dce17f97a53c523
lino <me@lino.berlin>
2021-12-15 18:48:57 +0100

    improve with new tailwind version

--- config/tailwind.dev.config.js
+++ config/tailwind.dev.config.js
@@ -1,10 +1,5 @@
-const config = require('@nunt/design/tailwind.config');
-
 module.exports = {
-  darkMode: config.darkMode,
-  purge: config.purge,
-  theme: config.theme,
-  corePlugins: config.corePlugins,
-  variants: config.variants,
-  plugins: config.plugins
+  presets: [
+    require('@nunt/design/tailwind.config')
+  ]
 };
--- config/tailwind.prod.config.js
+++ config/tailwind.prod.config.js
@@ -1,17 +1,5 @@
-const config = require('@nunt/design/tailwind.config.js');
-
 module.exports = {
-  darkMode: config.darkMode,
-  purge: {
-    enabled: true,
-    content: [
-      './src/**/*.html',
-      './src/**/*.ts',
-      './node_modules/@nunt/**/**/*.js'
-    ],
-  },
-  theme: config.theme,
-  corePlugins: config.corePlugins,
-  variants: config.variants,
-  plugins: config.plugins
+  presets: [
+    require('@nunt/design/tailwind.config')
+  ]
 };
