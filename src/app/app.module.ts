f3d47badbaf738e64cf1f1a2c1fe4f9e886b024f
lino <me@lino.berlin>
2022-01-21 10:40:04 +0100

    cleanup, minor improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ApplicationSetupProvider, NuntCmsModule, NuntCoreModule} from '@nunt/core';
+import {NuntCmsModule, NuntCoreModule} from '@nunt/core';
 import {NuntDesignModule} from '@nunt/design';
 import {RouterModule} from '@angular/router';
 import {routes} from './app.routes';
